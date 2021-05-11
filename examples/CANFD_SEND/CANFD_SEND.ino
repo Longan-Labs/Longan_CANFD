8036684d2b653a3588369857f0909a173d788377
Stephen Lo <36740855+stephen1874@users.noreply.github.com>
2021-05-11 12:17:35 +0800

    Update CANFD_SEND.ino

--- examples/CANFD_SEND/CANFD_SEND.ino
+++ examples/CANFD_SEND/CANFD_SEND.ino
@@ -1,10 +1,51 @@
-/*  MCP2517/8 send a can fd frame
+# Longan Labs CAN FD Library 
 
+Arduino library for MCP2517/MCP2518, it's available for most of theArduino boards, we test it with Arduino UNO, Leonardo, Mega as well as Zero.
+
+With this library, you can,
+
+1. Send a CAN2.0 frame
+2. Receive a CAN2.0 frame
+3. Send a CAN FD frame
+4. Send a CAN FD frame
+5. Set the masks and filters, there're 32 masks and filters. 
+
+## Installation
+
+1. [Download the library](https://github.com/Longan-Labs/Longan_CANFD/archive/refs/heads/master.zip)
+2. Extract the zip file
+3. In the Arduino IDe, navigate to Sketch > Include Library > Add .ZIP Library
+
+## Respository Contents
+
+* [**/examples**](./examples) - Example sketches for the library (.ino). Run these from the Arduino IDE.
+* [**/src**](./src) - Source files for the library (.cpp, .h).
+* [**keywords.txt**](./keywords.txt) - Keywords from this library that will be highlighted in the Arduino IDE.
+* [**library.properties**](./library.properties) - General library properties for the Arduino package manager.
+
+## Functions
+
+- begin()
+- init_Mask()
+- init_filt()
+- checkReceive()
+- readMsgBufID()
+- readMsgBuf()
+- getCanId()
+- sendMsgBuf()
+- isRemoteRequest()
+- isExtendedFrame()
+
+## Examples
+
+here are many examples implemented in this library. One of the examples is below. You can find other examples [here](./examples)
+
+```Arduino
+/*  MCP2517/8 send a can fd frame
     CAN FD Shield - https://www.longan-labs.cc/1030012.html
     CANBed FD - https://www.longan-labs.cc/1030009.html
     
     can-fd baud rate:
-
     CAN_125K_500K
     CAN_250K_500K
     CAN_250K_750K
@@ -73,3 +114,44 @@ void loop()
 }
 
 // END FILE
+```
+
+## Dev board by Longan Labs
+
+If you need a Dev board, plese try,
+
+- [CAN FD Shield for Arduino](https://www.longan-labs.cc/1030012.html)
+- [CANBed FD](https://www.longan-labs.cc/1030009.html)
+
+## License
+
+```
+MIT License
+
+Copyright (c) 2018 @ Longan Labs
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+```
+
+## Contact us
+
+If you have any question, please feel free to contact [support@longan-labs.cc](support@longan-labs.cc)
+
+
+[![Analytics](https://ga-beacon.appspot.com/UA-101965714-1/Longan_CANFD)](https://github.com/igrigorik/ga-beacon)
